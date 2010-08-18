775d7ae37de8d16606ec2a623257edb284d4b525
George Palmer <george.palmer@gmail.com>
2010-08-18 11:12:53 +0100

    Fix version checking to support 1.x.y versions

--- lib/couch_foo/database_version.rb
+++ lib/couch_foo/database_version.rb
@@ -5,30 +5,35 @@ module CouchFoo
 
   class DatabaseVersion
 
-    attr_accessor :major, :minor
+    attr_accessor :major, :minor, :patch
 
     def initialize(version)
       self.major = 0
       self.minor = 0
+      self.patch = 0
       self.version = version
     end
 
     def version=(version)
-      if version.to_s =~ /(\d)\.(\d+)/
+      if version.to_s =~ /(\d)\.(\d)\.(\d+)/
         self.major = $1.to_i
         self.minor = $2.to_i
+        self.patch = $3.to_i
       end
     end
 
     def > (version)
       v = DatabaseVersion.new(version)
-      (self.major > v.major) || ((self.major = v.major) && (self.minor > v.minor))
+      (self.major > v.major) || 
+      ((self.major == v.major) && (self.minor > v.minor)) ||
+      ((self.major == v.major) && (self.minor == v.minor) && (self.patch > v.patch))
     end
 
     def < (version)
       v = DatabaseVersion.new(version)
-      (v.major > self.major) || ((v.major = self.major) && (v.minor > self.minor))
+      (v.major > self.major) || 
+      ((v.major == self.major) && (v.minor > self.minor)) ||
+      ((v.major == self.major) && (v.minor == self.minor) && (v.patch > self.patch))
     end
-
   end
 end
